a6cf2551a89b4c84c1c037744a270bdcda578e9c
WaldenerLee <waldenerlee@gmail.com>
2020-12-03 21:47:36 +0800

    ...

--- captcha/src/main/java/com/waldener/captcha/TextSeekBar.java
+++ captcha/src/main/java/com/waldener/captcha/TextSeekBar.java
@@ -27,21 +27,21 @@ class TextSeekBar extends AppCompatSeekBar {
 
     public TextSeekBar(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        textPaint = new Paint();
-        textPaint.setTextAlign(Paint.Align.CENTER);
-        int textSize = Utils.dp2px(context, 14);
-        textPaint.setTextSize(textSize);
-        textPaint.setAntiAlias(true);
-        textPaint.setColor(Color.parseColor("#545454"));
+//        textPaint = new Paint();
+//        textPaint.setTextAlign(Paint.Align.CENTER);
+//        int textSize = Utils.dp2px(context, 14);
+//        textPaint.setTextSize(textSize);
+//        textPaint.setAntiAlias(true);
+//        textPaint.setColor(Color.parseColor("#545454"));
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        Paint.FontMetrics fontMetrics = textPaint.getFontMetrics();
-        float top = fontMetrics.top;//为基线到字体上边框的距离,即上图中的top
-        float bottom = fontMetrics.bottom;//为基线到字体下边框的距离,即上图中的bottom
-        int baseLineY = (int) (getHeight() / 2 - top / 2 - bottom / 2);//基线中间点的y轴计算公式
-        canvas.drawText("向右滑动滑块完成拼图", getWidth() / 2, baseLineY, textPaint);
+//        Paint.FontMetrics fontMetrics = textPaint.getFontMetrics();
+//        float top = fontMetrics.top;//为基线到字体上边框的距离,即上图中的top
+//        float bottom = fontMetrics.bottom;//为基线到字体下边框的距离,即上图中的bottom
+//        int baseLineY = (int) (getHeight() / 2 - top / 2 - bottom / 2);//基线中间点的y轴计算公式
+//        canvas.drawText("向右滑动滑块完成拼图", getWidth() / 2, baseLineY, textPaint);
     }
 }
