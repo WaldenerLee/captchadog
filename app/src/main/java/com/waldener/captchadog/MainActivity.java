99d2144dbfd1fa64430da2fcba23997d158c0105
WaldenerLee <waldenerlee@gmail.com>
2020-12-03 21:42:17 +0800

    ...

--- app/src/main/java/com/waldener/captchadog/MainActivity.java
+++ app/src/main/java/com/waldener/captchadog/MainActivity.java
@@ -3,12 +3,40 @@ package com.waldener.captchadog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.waldener.captcha.Captcha;
 
 public class MainActivity extends AppCompatActivity {
+    private Captcha captcha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        captcha = findViewById(R.id.captCha);
+
+        captcha.setBitmap("https://tse2-mm.cn.bing.net/th/id/OIP.gY_Umtnvx6nZQqwNnkVdcAHaE8?pid=Api&rs=1");
+
+        captcha.setCaptchaListener(new Captcha.CaptchaListener() {
+            @Override
+            public String onAccess(long time) {
+                Log.i("MainActivity", "onAccess " + time);
+                return null;
+            }
+
+            @Override
+            public String onFailed(int failCount) {
+                Log.i("MainActivity", "onFailed " + failCount);
+                return null;
+            }
+
+            @Override
+            public String onMaxFailed() {
+                Log.i("MainActivity", "onMaxFailed");
+                return null;
+            }
+        });
     }
 }
